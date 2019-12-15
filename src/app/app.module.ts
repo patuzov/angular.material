4928514d5af0476f81e308537fade2c490a70ff4
Pavel Tuzov <patuzov@microsoft.com>
2019-12-15 21:08:34 +0100

    main component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './shared/material/material.module';
 import { ButtonsComponent } from './buttons/buttons.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ButtonsComponent
+    ButtonsComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
